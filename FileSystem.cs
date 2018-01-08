dcdb76a01d09fd87e6c3192ce82bd26c4b9e5e21
David Neumaier <filosofem.dave@gmail.com>
2018-01-08 19:41:19 +0100

    init

---
+++ FileSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Qrakhen.Sqript;
+using System.Collections.Generic;
+using System.IO;
+
+namespace __Qrakhen.Sqript.Lib.FileSystem
+{
+    public class FileInterface : Interface
+    {
+        public FileInterface() : base("file") {
+
+        }
+
+        public Value exists(Dictionary<string, Value> parameters) {
+            return new Value(File.Exists(parameters["file"].str()), Qrakhen.Sqript.ValueType.BOOLEAN);
+        }
+
+        public Value read(Dictionary<string, Value> parameters) {
+            if (!File.Exists(parameters["file"].str())) throw new Qrakhen.Sqript.Exception("could not find file '" + parameters["file"] + "'");
+            else return new Value(File.ReadAllText(parameters["file"].str()), Qrakhen.Sqript.ValueType.STRING);
+        }
+
+        public Value write(Dictionary<string, Value> parameters) {
+            string content;
+            if (parameters["content"].getValue() == null) content = "";
+            else content = parameters["content"].str();
+            File.WriteAllText(parameters["file"].str(), content);
+            return Value.TRUE;
+        }
+
+        public override void load() {
+            define(new Call("read", new string[] { "file" }, read));
+            define(new Call("write", new string[] { "file", "content" }, write));
+            define(new Call("exists", new string[] { "file" }, exists));
+        }
+    }
+}
