3eeb220aa8b60607a1b71e07d000edb457c44bdb
kdanzer-tgm <44087538+kdanzer-tgm@users.noreply.github.com>
2022-03-06 23:04:02 +0100

    Updated Lib and Added

--- Console.cs
+++ Console.cs
@@ -1,34 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Qrakhen.Sqript;
 
-namespace Qrakhen.SqriptLib
-{
-    public class ConsoleInterface : Interface
-    {
-        public ConsoleInterface() : base("console") {
+namespace Qrakhen.SqriptLib {
+	public class ConsoleInterface : Interface {
 
-        }
+		private ConsoleColor _consoleColor = ConsoleColor.White;
 
-        public Value write(Dictionary<string, Value> parameters) {
-            Console.Write(parameters["value"].getValue());
-            return null;
-        }
+		public ConsoleInterface() : base("console") {
 
-        public Value read(Dictionary<string, Value> parameters) {
-            var key = Console.ReadKey();
-            return new Value((int) key.KeyChar, Sqript.ValueType.INTEGER);
-        }
+		}
 
-        public Value readLine(Dictionary<string, Value> parameters) {
-            var line = Console.ReadLine();
-            return new Value(line, Sqript.ValueType.STRING);
-        }
+		public Value setColor(Dictionary<string, Value> parameters) {
+			object value = parameters["color"].getValue();
+			if(value is string colorString) {
+				_consoleColor = (ConsoleColor) Enum.Parse(typeof(ConsoleColor), colorString, true);
+			} else if(value is int colorInt) {
+				_consoleColor = (ConsoleColor) colorInt;
+			} else {
+				throw new ArgumentException($"The parameter 'color' needs to be an {typeof(int)} or {typeof(string)}!");
+			}
+			return null;
+		}
 
-        public override void load() {
-            define(new Call("write", new string[] { "value" }, write, Sqript.ValueType.NULL));
-            define(new Call("read", new string[] { }, read, Sqript.ValueType.INTEGER));
-            define(new Call("readLine", new string[] { }, readLine, Sqript.ValueType.STRING));
-        }
-    }
+		public Value write(Dictionary<string, Value> parameters) {
+			Console.ForegroundColor = _consoleColor;
+			Console.Write(parameters["value"].getValue());
+			return null;
+		}
+
+		public Value writeln(Dictionary<string, Value> parameters) {
+			Console.ForegroundColor = _consoleColor;
+			string text = parameters["value"].getValue().ToString();
+			Console.Write(Regex.Unescape(text) + Environment.NewLine);
+			return null;
+		}
+
+		public Value readKey() {
+			var key = Console.ReadKey();
+			return new Value((int) key.KeyChar, Sqript.ValueType.Integer);
+		}
+
+		public Value read() {
+			var line = Console.ReadLine();
+			return new Value(line, Sqript.ValueType.String);
+		}
+
+		public override void load() {
+			define(new Call(write, new string[] { "value" }, Sqript.ValueType.Null));
+			define(new Call(writeln, new string[] { "value" }, Sqript.ValueType.Null));
+			define(new Call(readKey, Sqript.ValueType.Integer));
+			define(new Call(read, Sqript.ValueType.String));
+			define(new Call(setColor, new string[] { "color" }, Sqript.ValueType.Null));
+		}
+	}
 }
--- FileSystem.cs
+++ FileSystem.cs
@@ -3,35 +3,37 @@ using Qrakhen.Sqript;
 using System.Collections.Generic;
 using System.IO;
 
-namespace Qrakhen.SqriptLib
-{
-    public class FileInterface : Interface
-    {
-        public FileInterface() : base("file") {
+namespace Qrakhen.SqriptLib {
+	public class FileInterface : Interface {
+		public FileInterface() : base("file") {
 
-        }
+		}
 
-        public Value exists(Dictionary<string, Value> parameters) {
-            return new Value(File.Exists(parameters["file"].str()), Qrakhen.Sqript.ValueType.BOOLEAN);
-        }
+		public Value exists(Dictionary<string, Value> parameters) {
+			return new Value(File.Exists(parameters["file"].str()), Qrakhen.Sqript.ValueType.Boolean);
+		}
 
-        public Value read(Dictionary<string, Value> parameters) {
-            if (!File.Exists(parameters["file"].str())) throw new Qrakhen.Sqript.Exception("could not find file '" + parameters["file"] + "'");
-            else return new Value(File.ReadAllText(parameters["file"].str()), Qrakhen.Sqript.ValueType.STRING);
-        }
+		public Value read(Dictionary<string, Value> parameters) {
+			if(!File.Exists(parameters["file"].str()))
+				throw new Qrakhen.Sqript.Exception("could not find file '" + parameters["file"] + "'");
+			else
+				return new Value(File.ReadAllText(parameters["file"].str()), Qrakhen.Sqript.ValueType.String);
+		}
 
-        public Value write(Dictionary<string, Value> parameters) {
-            string content;
-            if (parameters["content"].getValue() == null) content = "";
-            else content = parameters["content"].str();
-            File.WriteAllText(parameters["file"].str(), content);
-            return Value.TRUE;
-        }
+		public Value write(Dictionary<string, Value> parameters) {
+			string content;
+			if(parameters["content"].getValue() == null)
+				content = "";
+			else
+				content = parameters["content"].str();
+			File.WriteAllText(parameters["file"].str(), content);
+			return Value.True;
+		}
 
-        public override void load() {
-            define(new Call("read", new string[] { "file" }, read, Sqript.ValueType.STRING));
-            define(new Call("write", new string[] { "file", "content" }, write, Sqript.ValueType.BOOLEAN));
-            define(new Call("exists", new string[] { "file" }, exists, Sqript.ValueType.BOOLEAN));
-        }
-    }
+		public override void load() {
+			define(new Call(read, new string[] { "file" }, Sqript.ValueType.String, "read"));
+			define(new Call(write, new string[] { "file", "content" }, Sqript.ValueType.Boolean, "write"));
+			define(new Call(exists, new string[] { "file" }, Sqript.ValueType.Boolean, "exists"));
+		}
+	}
 }
